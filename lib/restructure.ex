5a3ab0b9109bb70f0994bb02c575aa6917a2d2a7
Brett Beatty <brettbeatty@gmail.com>
2024-02-21 23:12:59 -0600

    Add support for named fields

--- lib/restructure.ex
+++ lib/restructure.ex
@@ -19,31 +19,142 @@ defmodule Restructure do
   end
 
   defp defstruct(env, name, fields) do
-    macro = build_macro(name, fields)
-    constructor = build_constructor(env, name, fields)
+    named_fields_var = check_fields(fields)
+    macro = build_macro(name, fields, named_fields_var)
+    constructor = build_constructor(env, name, fields, named_fields_var)
     fetcher = build_fetcher(env, name, fields)
+    named_field_tag = build_named_fields_tag(name, named_fields_var)
 
     quote do
       unquote(macro)
       unquote(constructor)
       unquote_splicing(fetcher)
+      unquote(named_field_tag)
     end
   end
 
-  defp build_macro(name, fields) do
+  defp check_fields(fields)
+
+  defp check_fields([{name, _meta, context} | fields]) when is_atom(name) and is_atom(context) do
+    check_fields(fields)
+  end
+
+  defp check_fields([named_fields]) when is_list(named_fields) do
+    if Keyword.keyword?(named_fields) do
+      Macro.unique_var(:named_fields, __MODULE__)
+    else
+      raise "named fields must be a keyword list"
+    end
+  end
+
+  defp check_fields([]) do
+    nil
+  end
+
+  defp build_macro(name, fields, named_fields_var) do
+    macro_args = build_macro_args(fields, named_fields_var)
+    constructor_args = build_constructor_args(fields, named_fields_var)
+    named_fields? = not is_nil(named_fields_var)
+
     quote do
-      defmacro unquote(name)(unquote_splicing(fields)) do
-        Restructure._build_structure(__CALLER__, __MODULE__, unquote(name), unquote(fields))
+      defmacro unquote(name)(unquote_splicing(macro_args)) do
+        Restructure._build_structure(
+          __CALLER__,
+          __MODULE__,
+          unquote(name),
+          unquote(constructor_args),
+          unquote(named_fields?)
+        )
       end
     end
   end
 
-  defp build_constructor(env, name, fields) do
+  defp build_macro_args(fields, named_fields_var) do
+    Enum.map(fields, fn
+      ordered_field when is_tuple(ordered_field) ->
+        ordered_field
+
+      named_fields when is_list(named_fields) ->
+        quote do
+          unquote(named_fields_var) \\ []
+        end
+    end)
+  end
+
+  def _build_structure(env, module, name, fields, named_fields?)
+
+  def _build_structure(env, module, name, fields, _named_fields?)
+      when env.context in [nil, :guard] do
     quote do
-      def unquote(:"RESTRUCTURE-#{name}")(unquote_splicing(fields)) do
-        unquote(build_wrapper(env.module, name, fields))
+      unquote(module).unquote(:"RESTRUCTURE-#{name}")(unquote_splicing(fields))
+    end
+  end
+
+  def _build_structure(env, module, name, fields, _named_fields? = false)
+      when env.context == :match do
+    build_wrapper(module, name, fields)
+  end
+
+  def _build_structure(env, module, name, fields, _named_fields? = true)
+      when env.context == :match do
+    build_wrapper(module, name, build_named_field_match(fields))
+  end
+
+  defp build_named_field_match(fields) do
+    List.update_at(fields, -1, fn
+      var when is_tuple(var) ->
+        var
+
+      pairs when is_list(pairs) ->
+        if Keyword.keyword?(pairs) do
+          {:%{}, [], pairs}
+        else
+          pairs
+        end
+    end)
+  end
+
+  defp build_constructor(env, name, fields, named_fields_var) do
+    args = build_constructor_args(fields, named_fields_var)
+    data = build_data(fields, named_fields_var)
+
+    quote do
+      def unquote(:"RESTRUCTURE-#{name}")(unquote_splicing(args)) do
+        unquote(build_wrapper(env.module, name, data))
       end
     end
+  end
+
+  defp build_constructor_args(fields, named_fields_var)
+
+  defp build_constructor_args(fields, named_fields_var) when is_tuple(named_fields_var) do
+    List.replace_at(fields, -1, named_fields_var)
+  end
+
+  defp build_constructor_args(fields, nil) do
+    fields
+  end
+
+  defp build_data(fields, named_fields_var)
+
+  defp build_data(fields, named_fields_var) when is_tuple(named_fields_var) do
+    List.update_at(fields, -1, fn named_fields ->
+      named_data =
+        Enum.map(named_fields, fn {key, default} ->
+          value =
+            quote do
+              Keyword.get(unquote(named_fields_var), unquote(key), unquote(default))
+            end
+
+          {key, value}
+        end)
+
+      {:%{}, [], named_data}
+    end)
+  end
+
+  defp build_data(fields, nil) do
+    fields
   end
 
   defp build_fetcher(env, name, fields) do
@@ -69,32 +180,46 @@ defmodule Restructure do
 
   defp build_fetcher_clauses(fields, next_underscores, prev_underscores, builder)
 
-  defp build_fetcher_clauses([field | fields], [underscore | next], prev, builder) do
+  defp build_fetcher_clauses([field | fields], [underscore | next], prev, builder)
+       when tuple_size(field) == 3 do
     {field_name, _meta, _context} = field
     clause = builder.(prev ++ [field | next], field_name, {:ok, field})
 
     [clause | build_fetcher_clauses(fields, next, [underscore | prev], builder)]
   end
 
+  defp build_fetcher_clauses([named_fields], [underscore], prev, builder)
+       when is_list(named_fields) do
+    clauses = build_named_fetcher_clauses(named_fields, prev, builder)
+    clauses ++ build_fetcher_clauses([], [], [underscore | prev], builder)
+  end
+
   defp build_fetcher_clauses([], [], prev, builder) do
     [builder.(prev, Macro.var(:_, __MODULE__), :error)]
+  end
+
+  defp build_named_fetcher_clauses(named_fields, ordered_fields, builder)
+
+  defp build_named_fetcher_clauses([{key, _default} | named_fields], underscores, builder) do
+    var = Macro.var(key, __MODULE__)
+
+    match =
+      quote do
+        %{unquote(key) => unquote(var)}
+      end
+
+    clause = builder.(underscores ++ [match], key, {:ok, var})
+
+    [clause | build_named_fetcher_clauses(named_fields, underscores, builder)]
+  end
+
+  defp build_named_fetcher_clauses([], _underscores, _builder) do
+    []
   end
 
   def fetch(struct, key) do
     %Restructure{module: module, name: name} = struct
     apply(module, :"RESTRUCTURE-#{name}-FETCH", [struct, key])
-  end
-
-  def _build_structure(env, module, name, fields)
-
-  def _build_structure(env, module, name, fields) when env.context in [nil, :guard] do
-    quote do
-      unquote(module).unquote(:"RESTRUCTURE-#{name}")(unquote_splicing(fields))
-    end
-  end
-
-  def _build_structure(env, module, name, fields) when env.context == :match do
-    build_wrapper(module, name, fields)
   end
 
   defp build_wrapper(module, name, fields) do
@@ -104,6 +229,16 @@ defmodule Restructure do
         module: unquote(module),
         name: unquote(name)
       }
+    end
+  end
+
+  defp build_named_fields_tag(name, named_fields_var) do
+    named_fields? = not is_nil(named_fields_var)
+
+    quote do
+      def unquote(:"RESTRUCTURE-#{name}-NAMED-FIELDS?")() do
+        unquote(named_fields?)
+      end
     end
   end
 
@@ -121,9 +256,45 @@ defmodule Restructure do
       %Restructure{data: data, module: module, name: name} = struct
 
       left = "#{Macro.inspect_atom(:literal, module)}.#{Macro.inspect_atom(:remote_call, name)}("
+      contents = data |> Tuple.to_list() |> inspect_data(module, name, opts)
       right = ")"
 
-      container_doc(left, Tuple.to_list(data), right, opts, &to_doc/2)
+      container_doc(left, contents, right, opts, &identity/2)
+    end
+
+    defp inspect_data(data, module, name, opts) do
+      named_fields? = apply(module, :"RESTRUCTURE-#{name}-NAMED-FIELDS?", [])
+      inspect_ordered_data(data, named_fields?, opts)
+    end
+
+    defp inspect_ordered_data(data, named_fields?, opts)
+
+    defp inspect_ordered_data([named_data], _named_fields? = true, opts) do
+      named_data
+      |> Enum.sort()
+      |> inspect_named_data(opts)
+    end
+
+    defp inspect_ordered_data([datum | ordered_data], named_fields?, opts) do
+      [to_doc(datum, opts) | inspect_ordered_data(ordered_data, named_fields?, opts)]
+    end
+
+    defp inspect_ordered_data([], _named_fields?, _opts) do
+      []
+    end
+
+    defp inspect_named_data(data, opts)
+
+    defp inspect_named_data([datum | data], opts) do
+      [Inspect.List.keyword(datum, opts) | inspect_named_data(data, opts)]
+    end
+
+    defp inspect_named_data([], _opts) do
+      []
+    end
+
+    defp identity(doc, _opts) do
+      doc
     end
   end
 end
--- test/restructure_test.exs
+++ test/restructure_test.exs
@@ -2,7 +2,7 @@ defmodule RestructureTest do
   use ExUnit.Case, async: true
   doctest Restructure
 
-  describe "positional fields only" do
+  describe "ordered fields only" do
     defmodule Coord do
       use Restructure
 
@@ -12,6 +12,7 @@ defmodule RestructureTest do
 
     test "returns a restructure struct" do
       import Coord
+
       assert %Restructure{} = coord_2d(0, 0)
     end
 
@@ -39,20 +40,79 @@ defmodule RestructureTest do
     end
   end
 
+  describe "named fields only" do
+    defmodule Bounds do
+      use Restructure
+
+      defstruct bounds(min: 0, max: 0)
+    end
+
+    test "returns a restructure struct" do
+      import Bounds
+
+      assert %Restructure{} = bounds()
+    end
+
+    test "allows matching all named" do
+      import Bounds
+
+      assert bounds(named) = bounds(min: -1, max: 1)
+      assert named == %{min: -1, max: 1}
+    end
+
+    test "allows matching a subset of named" do
+      import Bounds
+
+      assert bounds(max: max) = bounds(min: 0.0, max: 8.0)
+      assert max == 8.0
+    end
+
+    test "allows matching specific named" do
+      import Bounds
+
+      assert bounds(max: max, min: min) = bounds(min: -2, max: 3)
+      assert min == -2
+      assert max == 3
+    end
+
+    test "allows passing in named field variable" do
+      import Bounds
+
+      named = [max: 7]
+      assert bounds(named) == bounds(min: 0, max: 7)
+    end
+
+    test "handles values that are not their own AST" do
+      import Bounds
+
+      assert bounds(min: %{inclusive: min}) = bounds(min: %{inclusive: 4})
+      assert min == 4
+    end
+  end
+
   describe "fetching" do
     defmodule KV do
       use Restructure
 
-      defstruct pair(key, value)
+      defstruct pair(key, value, meta: %{}, id: make_ref())
     end
 
-    test "supports fetching fields from struct" do
+    test "supports fetching ordered fields from struct" do
       import KV
 
       struct = pair(:some_key, %{some: "value"})
       assert struct[:key] == :some_key
       assert struct[:value] == %{some: "value"}
       assert struct[:fake_field] == nil
+    end
+
+    test "supports fetching named fields from struct" do
+      import KV
+
+      struct = pair(:another_key, %{another: :value}, meta: %{one: [:more, "thing"]})
+      assert struct[:meta] == %{one: [:more, "thing"]}
+      assert is_reference(struct[:id])
+      assert struct[:one] == nil
     end
   end
 
@@ -80,6 +140,8 @@ defmodule RestructureTest do
       defstruct empty
       defstruct one_field(value)
       defstruct multi_field(a, b, c)
+      defstruct named_only(a: :b, c: %{d: "e"})
+      defstruct mixed(a, b, c: "c", d: "d")
     end
 
     test "can inspect empty struct" do
@@ -90,6 +152,7 @@ defmodule RestructureTest do
 
     test "can inspect structs with one field" do
       import Inspectable
+
       value = %{some: [:fairly, "complex"], nested: ~c"value"}
       struct = one_field(value)
 
@@ -98,10 +161,34 @@ defmodule RestructureTest do
 
     test "can inspect structs with multiple fields" do
       import Inspectable
+
       struct = multi_field(:some, {:arbitrary, "number", ~c"of"}, :fields)
 
       assert inspect(struct) ==
                ~S[RestructureTest.Inspectable.multi_field(:some, {:arbitrary, "number", ~c"of"}, :fields)]
+    end
+
+    test "can inspect structs with only named fields and include defaults" do
+      import Inspectable
+
+      struct = named_only()
+      assert inspect(struct) == ~S[RestructureTest.Inspectable.named_only(a: :b, c: %{d: "e"})]
+    end
+
+    test "can inspect named-only structs with overrides" do
+      import Inspectable
+
+      struct = named_only(c: ?r, a: ?p)
+      assert inspect(struct) == "RestructureTest.Inspectable.named_only(a: 112, c: 114)"
+    end
+
+    test "can inspect structs with a mix of ordered and named fields" do
+      import Inspectable
+
+      struct = mixed(%{letter: "a"}, B, c: "see?")
+
+      assert inspect(struct) ==
+               ~S[RestructureTest.Inspectable.mixed(%{letter: "a"}, B, c: "see?", d: "d")]
     end
   end
 end
